fde819f13900dcd8c2ab7ad445a2b697f10ece85
erdigvijaysingh <dpdigvijaysingh@gmail.com>
2023-06-02 15:31:50 +0530

    First
    First project on git hub

---
+++ IdentityTable/Auth/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using IdentityTable.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityTable.Auth
+{
+    public class ApplicationDbContext:IdentityDbContext<ApplicationUser>
+    {
+        
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+        //protected override void OnModelCreating(ModelBuilder builder)
+        //{
+        //    base.OnModelCreating(builder);
+        //}
+        public DbSet<AnUserRegister> Employees{ get; set;}
+        public DbSet<Product> Product { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+    }
+}
